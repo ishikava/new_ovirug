5e75fd49f7c3049b03bf0a80d9784c289ddbd356
Maksim Kiselev (Work) <magnumru@yandex.ru>
2019-12-10 11:16:43 +0300

    fix

--- templates/_smev/snils_request.php
+++ templates/_smev/snils_request.php
@@ -1,59 +1,24 @@
 <?php
 
 $content = '<?xml version="1.0" encoding="UTF-8"?>
-<s:Envelope
-        xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
-        xmlns:ns="urn://x-artefacts-smev-gov-ru/services/message-exchange/types/1.1"
-        xmlns:ns2="urn://x-artefacts-smev-gov-ru/services/message-exchange/types/basic/1.1"
-        xmlns:ns3="urn://x-artefacts-smev-gov-ru/services/message-exchange/types/faults/1.1"
-        xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
-    <s:Body>
-        <ns:SendRequestRequest>
-            <ns:SenderProvidedRequestData Id="SIGNED_BY_CONSUMER" wsu:Id="SIGNED_BY_CONSUMER">
-                <ns:MessageID>'.$data['MessageID'].'</ns:MessageID>
-                <ns2:MessagePrimaryContent>
-                    <tns:SnilsByAdditionalDataRequest xmlns:tns="http://kvs.pfr.com/snils-by-additionalData/1.0.1"
-                                                      xmlns:smev="urn://x-artefacts-smev-gov-ru/supplementary/commons/1.0.1"
-                                                      xmlns:pfr="http://common.kvs.pfr.com/1.0.0">
-                        <smev:FamilyName>'.$data['FamilyName'].'</smev:FamilyName>
-                        <smev:FirstName>'.$data['FirstName'].'</smev:FirstName>
-                        <smev:Patronymic>'.$data['Patronymic'].'</smev:Patronymic>
-                        <tns:BirthDate>'.$data['BirthDate'].'</tns:BirthDate>
-                        <tns:Gender>'.$data['Gender'].'</tns:Gender>
-                        <tns:BirthPlace>
-                            <pfr:PlaceType>'.$data['BirthPlace']['PlaceType'].'</pfr:PlaceType>
-                            <pfr:Settlement>'.$data['BirthPlace']['Settlement'].'</pfr:Settlement>
-                            <pfr:District>'.$data['BirthPlace']['District'].'</pfr:District>
-                            <pfr:Region>'.$data['BirthPlace']['Region'].'</pfr:Region>
-                            <pfr:Country>'.$data['BirthPlace']['Country'].'</pfr:Country>
-                        </tns:BirthPlace>
-                        <smev:PassportRF>
-                            <smev:Series>'.$data['PassportRF']['Series'].'</smev:Series>
-                            <smev:Number>'.$data['PassportRF']['Number'].'</smev:Number>
-                            <smev:IssueDate>'.$data['PassportRF']['IssueDate'].'</smev:IssueDate>
-                            <smev:Issuer>'.$data['PassportRF']['Issuer'].'</smev:Issuer>
-                        </smev:PassportRF>
-                    </tns:SnilsByAdditionalDataRequest>
-                </ns2:MessagePrimaryContent>
-            </ns:SenderProvidedRequestData>
-            <ns:CallerInformationSystemSignature>
-                <ds:Signature xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
-                    <ds:SignedInfo>
-                        <ds:CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
-                        <ds:SignatureMethod
-                                Algorithm="urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34102012-gostr34112012-256"/>
-                        <ds:Reference URI="#SIGNED_BY_CONSUMER">
-                            <ds:Transforms>
-                                <ds:Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
-                            </ds:Transforms>
-                            <ds:DigestMethod Algorithm="urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34112012-256"/>
-                            <ds:DigestValue></ds:DigestValue>
-                        </ds:Reference>
-                    </ds:SignedInfo>
-                    <ds:SignatureValue></ds:SignatureValue>
-                    <ds:KeyInfo></ds:KeyInfo>
-                </ds:Signature>
-            </ns:CallerInformationSystemSignature>
-        </ns:SendRequestRequest>
-    </s:Body>
-</s:Envelope>';
+<tns:SnilsByAdditionalDataRequest xmlns:tns="http://kvs.pfr.com/snils-by-additionalData/1.0.1" xmlns:smev="urn://x-artefacts-smev-gov-ru/supplementary/commons/1.0.1" xmlns:pfr="http://common.kvs.pfr.com/1.0.0">
+   <smev:FamilyName>ИВАНОВ</smev:FamilyName>
+   <smev:FirstName>ИВАН</smev:FirstName>
+   <smev:Patronymic>ИВАНОВИЧ</smev:Patronymic>
+   <tns:BirthDate>1967-05-21</tns:BirthDate>
+   <tns:Gender>Male</tns:Gender>
+   <tns:BirthPlace>
+      <pfr:PlaceType>ОСОБОЕ</pfr:PlaceType>
+      <pfr:Settlement>ЗАГОРСК</pfr:Settlement>
+      <pfr:District>ЛЕНИНСКИЙ</pfr:District>
+      <pfr:Region>МОСКОВСКАЯ ОБЛАСТЬ</pfr:Region>
+      <pfr:Country>РФ</pfr:Country>
+   </tns:BirthPlace>
+   <smev:PassportRF>
+      <smev:Series>0005</smev:Series>
+      <smev:Number>777777</smev:Number>
+      <smev:IssueDate>1986-06-13</smev:IssueDate>
+      <smev:Issuer>ОВД</smev:Issuer>
+   </smev:PassportRF>
+</tns:SnilsByAdditionalDataRequest>
+';
