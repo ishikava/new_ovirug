48a802268f7f03e26c25d07698ef7c06cc63e080
Maksim Kiselev (Work) <magnumru@yandex.ru>
2019-12-13 12:43:33 +0300

    fix

--- sh_tests/snils_result.sh
+++ sh_tests/snils_result.sh
@@ -2,4 +2,4 @@
 echo '' > output.xml;
 echo -e '========================================================\n' >> output.xml;
 
-curl http://127.0.0.1/rsmev/snils/getresult?envelope >> output.xml -d '' ;
+curl -X GET http://127.0.0.1/rsmev/snils/getresult?envelope >> output.xml -d '' ;
